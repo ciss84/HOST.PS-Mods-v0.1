42b2ca7d647ed27412c0e375852e5ca2c59b8719
ciss84 <37186888+ciss84@users.noreply.github.com>
2019-12-25 02:21:02 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,135 @@
+var ICONSZ = 150;
+var PADDING = 20;
+
+var xmbVue = new Vue({
+  el: "#xmb",
+  data: model,
+  methods: {
+    handleKey: function handleKey(dir, val) {
+      this.cursor[dir] += val;
+      var nCols = this.nColumns;
+      // wrap x
+      this.cursor.x = this.cursor.x % nCols;
+      if (this.cursor.x < 0) {
+        this.cursor.x = this.cursor.x + nCols;
+      }
+
+      //wrap y
+      var nRows = this.nRows;
+      this.cursor.y = this.cursor.y % nRows;
+      if (this.cursor.y < 0) {
+        this.cursor.y = this.cursor.y + nRows;
+      }
+
+      this.highlightCell(this.cursor.x, this.cursor.y);
+    },
+    highlightCell: function highlightCell(column, row) {
+
+      console.log(column, row);
+	  //alert(column, row);
+      //update position of elements as well
+      var xAccum = (-column - 1) * (ICONSZ);
+      if (column == 0) {
+        xAccum += ICONSZ;
+      }
+      var yAccum;
+
+      _.each(this.columns, function (col, colKey) {
+        col.active = false;
+        yAccum =  - (ICONSZ) * (row + 1);
+
+        col.position.x = xAccum;
+        xAccum += ICONSZ;
+        if (column === col.index || column === col.index + 1) {
+          xAccum += ICONSZ;
+        }
+
+        _.each(col.items, function (item, rowN) {
+          if (rowN == row && col.index == column) {
+            item.active = true;
+            col.active = true;
+          } else {
+            item.active = false;
+          }
+
+          if (rowN == row) {
+            yAccum += ICONSZ;
+          }
+          yAccum += ICONSZ;
+          item.position.y = yAccum;
+        });
+      });
+      this.cursor.y = row;
+      this.cursor.x = column;
+    }
+  },
+  watch: {
+    cursor: function cursor(e) {
+      console.log('cursor mutated', e);
+    }
+  },
+  computed: {
+    nColumns: function nColumns() {
+      return Object.keys(this.columns).length;
+    },
+    nRows: function nRows() {
+      //get the row at the current index
+      var row = this.columnsArray[this.cursor.x];
+      if (!row) {
+        console.log('invalid row index: ', this.cursor.x);
+        return 0;
+      }
+      return row.items.length; //todo: number of columns in this row
+    },
+    columnsArray: function columnsArray() {
+      var _this = this;
+
+      //get columns in an array
+      var arr = [];
+      Object.keys(this.columns).forEach(function (key) {
+        arr.push(_this.columns[key]);
+      });
+      return _.sortBy(arr, 'index');
+    }
+  },
+  created: function created() {
+    _.each(this.columns, function (column) {
+      _.each(column.items, function (item) {
+        item.active = false;
+      });
+    });
+    this.highlightCell(this.cursor.x, this.cursor.y);
+  }
+});
+
+$('body').on("mousewheel", _.debounce(scrollHandler, 50));
+
+function scrollHandler(e) {
+  console.log(e);
+  if (e.deltaX) {
+    xmbVue.handleKey('x', Math.sign(e.deltaX));
+  }
+  if (e.deltaY) {
+    xmbVue.handleKey('y', Math.sign(e.deltaY));
+  }
+}
+// REMAP TO D-PAD EXPERIMENTAL
+// 27 = O, 112 = /\, 113 = [], 114 = options, 
+// 116 = L1, 117 = R1, 118 = L2, 119 = R2, 120 = L3, 121 = R3,
+// 37 = left dpad, 38 = up dpad, 39 = right dpad, 40 = down dpad,
+window.onkeyup = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+   if (key == 37) {
+   xmbVue.handleKey('x', -1);
+   } else if (key == 39) {
+   xmbVue.handleKey('x', 1);
+   } else if (key  == 38) {
+   xmbVue.handleKey('y', -1);
+   } else if (key  == 40) {
+   xmbVue.handleKey('y', 1);
+   } else if (key == 116) {
+   window.open("payloads/hen18vrmod.html", "content");
+   } else if (key == 117) {
+   window.open("payloads/ftprw.html", "content");
+   }
+};
